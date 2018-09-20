33cfdfd2507853187c2766a5e5c4fe207c3499af
Egg3234 <nospam@gmail.com>
2018-09-20 09:37:23 -0500

    committed HelloGit project

---
+++ HelloGit/src/helloGit/HelloGit.java
@@ -0,0 +1,14 @@
+package helloGit;
+/*
+ * Egg3234
+ * 9-20-18
+ * Class to test linkage to github
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
